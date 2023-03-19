7df55cd62c3b6fceb78dccfbbd9b73737e3db95e
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-03-19 11:46:20 +0300

    Storybook:Task

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,35 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+
+import {AddItemForm} from "../AddItemForm";
+import {action} from "@storybook/addon-actions";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import {Task} from "../Task";
+import {TasksType} from "../Todolist";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TODOLIST/Task',
+  component: AddItemForm,
+  args:{
+    removeTasks: action('removeTasks'),
+    changeCheckbox:action('changeCheckbox'),
+    updateTask: action('updateTask'),
+    task:{id:'111',title:'JS',isDone: false},
+  }
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof AddItemForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+export const TaskIsNotDoneStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+export const TaskIsDoneStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+TaskIsDoneStory.args = {
+  task:{id:'111',title:'JS',isDone: true},
+};
